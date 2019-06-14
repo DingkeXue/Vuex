4768b873f55931f372f9c7226234141ebb917313
DingkeXue <605534423@qq.com>
2019-06-14 16:40:01 +0800

    添加样式

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="header">
+        <h2>Vue Demo</h2>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .header {
+        background: tomato;
+        color: white;
+        text-align: center;
+        margin: 0 auto;
+        padding: 8px 0;
+    }
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="todo-item">
+        <div class="item" :class="{'completed': todo.complete}">
+            <input type="checkbox" @click="toggle">
+            <span>{{todo.title}}</span>
+            <button class="remove" @click="$emit('Remove', todo.id)">remove</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export  default {
+        props: {
+            todo: {
+                type: Object
+            }
+        },
+        methods: {
+            toggle() {
+                this.todo.complete = !this.todo.complete;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .item {
+        width: 100%;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .remove {
+        background: tomato;
+        color: #fff;
+        border-radius: 3px;
+        float: right;
+    }
+
+    .completed {
+        text-decoration: line-through;
+        color: #999;
+    }
+</style>
